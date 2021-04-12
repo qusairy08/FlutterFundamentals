a43012e02f36ef863ba01d70eae975d332858e71
alqusairy <@alqusairy08>
2021-04-12 16:47:51 +0700

    list & list view

---
+++ navigasi_dan_multifungsi/android/app/src/main/kotlin/com/example/navigasi_dan_multifungsi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigasi_dan_multifungsi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ navigation_exampels/android/app/src/main/kotlin/com/example/navigation_exampels/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigation_exampels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
